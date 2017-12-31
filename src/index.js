ff3deb259e2eb3b57a468c9138be108fb28329f0
Seth Stephens <relative@airmail.cc>
2017-12-31 05:42:39 -0500

    Hide the console window

--- src/index.js
+++ src/index.js
@@ -7,10 +7,26 @@
  * 
  */
 const chalk = require('chalk')
+const config = require('./config')
 const logger = require('./Logger')
 const ms = require('ms')
+const os = require('os')
 const tick = require('./tick')
 const _payloads = require('./payloads')
+
+if (!config.debug) {
+  if (os.platform() === 'win32') {
+    try {
+      const wasw = require('windows-api-show-window')
+      wasw.hideCurrentProcessWindow().then(() => logger.info('Hid the window'))
+        .catch((err) => {
+          logger.error('Could not hide the console window', err)
+        })
+    } catch(err) {
+      logger.error('Couldn\'t require windows-api-show-window!', err)
+    }
+  }
+}
 
 logger.debug(`Running in ${chalk.bold.magenta('debug')} mode`)
 
