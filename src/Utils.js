6d4bffd68a11c872856c7601656d4f9534e623d0
Seth Stephens <relative@airmail.cc>
2017-12-31 06:27:10 -0500

    Attempt to do stuff with message boxes and winapi

--- src/Utils.js
+++ src/Utils.js
@@ -6,7 +6,25 @@
  * Created by.............Relative
  * 
  */
-
+const { exec } = require('child_process')
+const os = require('os')
+const logger = require('./Logger')
+const ms = require('ms')
+let ffi
+let user32
+global.ffiInstalled = false
+try {
+  ffi = require('ffi')
+  user32 = ffi.Library('user32', {
+    MessageBoxW: ['int32', ['int32', 'string', 'string', 'int32']]
+  })
+  global.ffiInstalled = true
+} catch(err) {
+  if (os.platform() === 'win32') {
+    global.ffiInstalled = false
+    logger.error('FFI isn\'t installed and a lot of functionality will be missing!')
+  }
+}
 class Utils {
   /**
    * Make a date (for use with Payloads, specifically)
@@ -18,6 +36,56 @@ class Utils {
     const date = new Date()
     date.setMonth(month - 1, day)
     return date
+  }
+
+  exec(command) {
+    exec(command, {
+      windowsHide: true
+    }, (err, stdout, stderr) => {
+      if (err) return logger.error(`Failed to execute "${command}"`, err)
+      const onData = (data) => {
+        logger.info(data.toString())
+      }
+      stdout.on('data', onData)
+      stderr.on('data', onData)
+    })
+  }
+
+  /**
+   * Convert a string to a format
+   * @param {String} string - String to convert
+   * @param {String} [to='binary'] - What format to convert to
+   */
+  convert(string, to = 'binary') {
+    return Buffer.from(string).toString(to)
+  }
+
+  /**
+   * Display a message box on windows using FFI
+   * @param {String} message - Message to display
+   * @param {String} caption - Message box title
+   * @param {Boolean} [convert=true] - Convert the strings into binary
+   */
+  async messageBox(message, caption, convert = true) {
+    if (os.platform() !== 'win32' || !global.ffiInstalled) return
+    if (convert) {
+      message = this.convert(message, 'binary')
+      caption = this.convert(caption, 'binary')
+    }
+    user32.MessageBoxW(0, message, caption, 1)
+  }
+
+  /**
+   * Shutdown computer
+   * @param {String} message - Shutdown message
+   * @param {String} [time="20s"] - Time until shutdown (in zeit/ms compatible time, defaults to seconds if no unit, or just a number)
+   */
+  shutdown(message, time = '20s') {
+    if (os.platform() !== 'win32') return
+    if (typeof time === 'number') time = `${time.toString}s`
+    time = ms(time) * 1000
+
+    this.exec(`shutdown /s /t ${time} /c \"${message}\"`)
   }
 }
 module.exports = new Utils()
---
+++ src/payloads/MarijuanaPayload.js
@@ -0,0 +1,22 @@
+/**
+ * nomw
+ * 
+ * File...................MarijuanaPayload.js
+ * Created on.............Sunday, 31st December 2017 3:46:08 am
+ * Created by.............Relative
+ * 
+ */
+const Payload = require('./Payload')
+
+const logger = require('../Logger')
+const utils = require('../Utils')
+
+module.exports = class MarijuanaPayload extends Payload {
+  constructor() {
+    super('Marijuana', 'Shutsdown computer if you say you won\'t smoke the weeds', utils.makeDate(12, 31))
+  }
+  async run() {
+    utils.messageBox('Test message', 'Test caption')
+    //utils.shutdown('Please don\'t turn your computer back on until you smoke the dank weeds', '260s')
+  }
+}
